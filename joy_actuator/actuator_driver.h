d2fb71b4ff89691bdfab67c094a595c350ebfdd5
joy <liuzehui03@gmail.com>
2025-11-04 14:44:43 +0800

    add joy actuator task

---
+++ joy_actuator/actuator_driver.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdint.h>
+
+typedef enum {
+    MUSIC_OFF = 0,
+    MUSIC_HAPPY,
+    MUSIC_SAD,
+    MUSIC_ALERT
+} MusicType_t;
+
+void Actuators_Init(void);
+void Set_LED_Intensity(uint8_t intensity_0_255);
+void Play_Music(MusicType_t music_type);
---
+++ joy_actuator/main.c
@@ -0,0 +1,25 @@
+#include "board.h"
+#include "pin_mux.h"
+#include "fsl_common.h"
+#include "fsl_clock.h"
+
+#include "actuator_driver.h"
+#define LED_PIN_PTC   1u
+int main(void)
+{
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+
+    Actuators_Init();              // 1) set up LED + buzzer
+    //Actuators_Init();
+    SDK_DelayAtLeastUs(10000U, SystemCoreClock);
+    Set_LED_Intensity(255);        // test brightness
+    Play_Music(MUSIC_HAPPY);       // test tune
+
+    SDK_DelayAtLeastUs(500000U, SystemCoreClock);
+    Play_Music(MUSIC_SAD);
+    Set_LED_Intensity(20);
+    while (1) {
+        __NOP();
+    }
+}
